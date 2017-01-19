91b8a37ff4f56d55ff7378ddc240b6ff0366b076
ant76 <trusov.andrew@gmail.com>
2017-01-19 15:57:30 +0400

    Initial commit

---
+++ src/main/java/ru/sam/habr/Application.java
@@ -0,0 +1,16 @@
+package ru.sam.habr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+@SpringBootApplication
+@ComponentScan
+@EnableJpaRepositories(basePackages = "ru.sam.habr")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
